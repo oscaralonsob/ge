666bc1f25b8e94487e34ab23e3a6db1184bfde08
Hadiur <oscaralonsob@gmail.com>
2022-12-22 08:03:11 +0100

    Include libraries and checking everything works correctly

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 compile:
-	g++ -Wall -std=c++17 src/*.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -llua5.3 -o build/gameengine
+	g++ -Wall -std=c++17 -I"./libs" src/*.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -llua5.3 -o build/gameengine
 
 run:
 	./build/gameengine
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+#include "../libs/glm/glm.hpp" //TODO: fix <> and ""
+#include "../libs/imgui/imgui.h"
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    glm::vec2 velocity = glm::vec2(5.0, 2.0);
+    
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
